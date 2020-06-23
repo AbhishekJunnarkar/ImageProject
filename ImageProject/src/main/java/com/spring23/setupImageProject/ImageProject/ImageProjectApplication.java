85f0795bec57dcbcf81c5ab505607c831792ce53
Abhishek Junnarkar <abhishek.junnarkar@ing.com>
2020-06-23 17:45:11 +0200

    initial commit

---
+++ ImageProject/src/main/java/com/spring23/setupImageProject/ImageProject/ImageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring23.setupImageProject.ImageProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProjectApplication.class, args);
+	}
+
+}
